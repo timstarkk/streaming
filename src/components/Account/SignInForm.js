d31a969625ab37e894faa47f62de69e06eb04506
timstarkk <tim@timstark.dev>
2022-01-31 17:46:13 -0600

    refactor signinform as functional component/fix

--- src/components/Account/SignInForm.js
+++ src/components/Account/SignInForm.js
@@ -1,101 +1,81 @@
-import React, { Component } from 'react';
+import React, { Component, useContext } from 'react';
+import { Navigate } from "react-router-dom";
 import { Auth } from 'aws-amplify';
 import './Account.css';
-import { ItemContext } from '../../context';
+import { AuthContext } from '../../authContext';
 
-export default class SignInForm extends Component {
-    static contextType = ItemContext;
-    constructor(props) {
-        super(props);
+export default function SignInForm() {
+    const [username, setUsername] = React.useState('');
+    const [password, setPassword] = React.useState('');
+    const [navigate, setNavigate] = React.useState(null);
 
-        this.state = {
-            username: '',
-            password: '',
-            setCurrentUser: '',
-            redirect: null
+    const {setCurrentUser} = useContext(AuthContext);
+
+
+    async function handleSubmit(e) {
+        e.preventDefault(); // make sure this is necessary
+        
+        await Auth.signIn({
+            username,
+            password
+        })
+            .then(() => console.log('signed in'))
+            .catch(error => {
+                if(error.code = "UserNotConfirmedException") {
+                        try {
+                            Auth.resendSignUp(username);
+                            console.log('code resent succesfully');
+                            setNavigate('/account/confirm');
+                        } catch (err) {
+                            console.log('error resending code: ', err);
+                        }
+                }
+            });
+
+
+        await Auth.confirmSignIn(username)
+            .then(() => console.log('confirmed sign up'))
+            .catch(error => console.log(error))
+
+        await Auth.currentSession()
+            .then(data => {
+                let userInfo = data.accessToken.payload;
+                setCurrentUser(userInfo);
+            })
+            .catch(err => {
+                console.log(err.message);
+            });
+
+        setNavigate('/');
+    }
+
+    function handleChange(e) {
+        if(e.target.name == 'Username') {
+            setUsername(e.target.value);
+        } else {
+            setPassword(e.target.value);
         }
 
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-
-    async handleSubmit(e) {
-        console.log('you clicked submit');
-        // const { setCurrentUser } = this.context;
-        // this.setState({
-        //     setCurrentUser
-        // })
-
-        // e.preventDefault();
-        // const { username, password, email, phone_number } = this.state;
-        // await Auth.signIn({
-        //     username,
-        //     password
-        // })
-        //     .then(() => console.log('signed in'))
-        //     .catch(error => {
-        //         if(error.code = "UserNotConfirmedException") {
-        //                 try {
-        //                     Auth.resendSignUp(username);
-        //                     console.log('code resent succesfully');
-        //                     this.setState({
-        //                         redirect: '/account/confirm' 
-        //                     })
-        //                 } catch (err) {
-        //                     console.log('error resending code: ', err);
-        //                 }
-        //         }
-        //     })
-
-
-        // await Auth.confirmSignIn(username)
-        //     .then(() => console.log('confirmed sign up'))
-        //     .catch(error => console.log(error))
-
-        // await Auth.currentSession()
-        //     .then(data => {
-        //         let userInfo = data.accessToken.payload;
-        //         this.state.setCurrentUser(userInfo);
-        //     })
-        //     .catch(err => {
-        //         console.log(err.message);
-        //     });
-
-        // this.setState({
-        //     redirect: "/"
-        // });
-    }
-
-    handleChange(e) {
-        // this.setState({
-        //     [e.target.name]: e.target.value
-        // });
-    }
-
-    render() {
-
-        if (this.state.redirect) {
-            // return <Redirect to={this.state.redirect} />
-            console.log('trying to redirect... not really');
-        
-        } else {
-            return (
-                <div className="account-wrapper">
-                    <div className="account-section">
-                        <div className="container-wrapper">
-                            <h4>Sign In To Your Account</h4>
-                            <div className="line" />
-                            <form onSubmit={this.handleSubmit}>
-                                <input type="text" name="username" placeholder="username" onChange={this.handleChange} />
-                                <input type="password" name="password" placeholder="password" onChange={this.handleChange} />
-                                <button>Sign In</button>
-                            </form>
-                            <a href="/account/signup">create an account</a>
-                        </div>
+    if (navigate) {
+        return <Navigate to={navigate} />
+    } else {
+        return (
+            <div className="account-wrapper">
+                <div className="account-section">
+                    <div className="container-wrapper">
+                        <h4>Sign In To Your Account</h4>
+                        <div className="line" />
+                        <form onSubmit={handleSubmit}>
+                            <input type="text" name="Username" placeholder="username" onChange={handleChange} />
+                            <input type="password" name="Password" placeholder="password" onChange={handleChange} />
+                            <button>Sign In</button>
+                        </form>
+                        <a href="/account/signup">create an account</a>
                     </div>
                 </div>
-            )
-        }
+            </div>
+        )
     }
 }
